35643f053eed10a2fd11766ccb24a1a4b8742630
Darya Lutuva <lutowa.darya@yandex.ru>
2020-08-03 16:23:04 +0300

    fixed: значения alt, add-button, форматирование, footer, close-button, удалена лишняя обертка блока profile

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton = popup.querySelector('.popup__save-button');
+let popup = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.edit-button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupSaveButton = popup.querySelector('.popup__save-button');
 let InfoContainer = document.querySelector('.profile-info');
 let saveButton = popup.querySelector('.popup__save-button');
 let name = document.querySelector('.profile-info__name');
 let aboutYou = document.querySelector('.profile-info__activity');
-let inputName = popup.querySelector('.popup__input-name');
-let inputAboutYou = popup.querySelector('.popup__input-activity');
+let inputName = popup.querySelector('.popup__form_input-name');
+let inputAboutYou = popup.querySelector('.popup__form_input-activity');
 
 inputName.value = name.textContent;
 inputAboutYou.value = aboutYou.textContent;
