f447c180f8df5c137045e31a8e7afd73b93414d4
Darya Lutuva <lutowa.darya@yandex.ru>
2020-08-21 11:07:13 +0300

    fixed: разбить функцию создания и добавления карточки на 2 функции

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const popup = document.querySelector('.popup');
-const popupAddCard = document.querySelector('.popup__add-card');
+const popupAddCard = document.querySelector('.popup_add-card');
 const popupOpenButton = document.querySelector('.edit-button');
 const popupAddCardOpenButton = document.querySelector('.profile__add-button');
 const popupAddCardCloseButton = document.querySelector('.popup__close-button_card');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 const popupSaveButton = popup.querySelector('.popup__save-button');
-const infoContainer = document.querySelector('.profile-info');
-let name = document.querySelector('.profile-info__name');
-let aboutYou = document.querySelector('.profile-info__activity');
-let inputName = popup.querySelector('.popup__form_input-name');
-let inputAboutYou = popup.querySelector('.popup__form_input-activity');
+const name = document.querySelector('.profile-info__name');
+const aboutYou = document.querySelector('.profile-info__activity');
+const inputName = popup.querySelector('.popup__form_input-name');
+const inputAboutYou = popup.querySelector('.popup__form_input-activity');
 const popupCardSaveButton = document.querySelector('.popup__save-button_card');
 
 //каточки на станице
@@ -43,11 +42,12 @@ const initialCards = [
 const cardsContainer = document.querySelector('.places');
 
 //функция добавленя элементов массива в DOM   
-function addCard(nameElement, linkElement) {
+function createCard(nameElement, linkElement) {
     const cardTemplate = document.querySelector('.place').content;
     const cardElement = cardTemplate.cloneNode(true);
+    const placeImage = cardElement.querySelector('.place__image');
       
-    cardElement.querySelector('.place__image').src = linkElement;
+    placeImage.src = linkElement;
     cardElement.querySelector('.place__title').textContent = nameElement;
 
     //Лайк карточки
@@ -57,27 +57,30 @@ function addCard(nameElement, linkElement) {
   
     //Удаление карточки 
     cardElement.querySelector('.place__delete').addEventListener('click', function(event) {
-        event.target.parentElement.remove();
-        
+        event.target.parentElement.remove();        
     });
 
     //  попап с фотографией
-    const popupPhoto = document.querySelector('.popup__photo')
-    cardElement.querySelector('.place__image').addEventListener('click', function (event) {
+    const popupPhoto = document.querySelector('.popup_photo');
+    
+    function openPopupPhoto() {
         popupPhoto.classList.toggle('popup_opened');
         popupPhoto.querySelector('.popup__title-photo').textContent = nameElement;
-        console.log(nameElement);
         popupPhoto.querySelector('.popup__image').src = linkElement;
-        console.log(linkElement);
-    });
+    }
+    
+    placeImage.addEventListener('click', openPopupPhoto);
 
     popupCloseButtonPhoto = popupPhoto.querySelector('.popup__close-button_photo');
     const popupClosePhoto = function () {
         popupPhoto.classList.remove('popup_opened');
     }
     popupCloseButtonPhoto.addEventListener('click', popupClosePhoto);
+    return cardElement;    
+}
 
-    cardsContainer.prepend(cardElement);
+function addCard(nameElement, linkElement){
+    cardsContainer.prepend(createCard(nameElement, linkElement));
 }
 
 initialCards.forEach(function (item) {
@@ -89,8 +92,8 @@ initialCards.forEach(function (item) {
 //Добавление карточки
 popupAddCard.addEventListener('submit', evt => {
     evt.preventDefault();
-    nameElement = popupAddCard.querySelector('.popup__form_input-placename').value;
-    linkElement = popupAddCard.querySelector('.popup__form_input-link').value;
+    const nameElement = popupAddCard.querySelector('.popup__form_input-placename').value;
+    const linkElement = popupAddCard.querySelector('.popup__form_input-link').value;
     
     addCard(nameElement, linkElement);
     popupAddCard.reset();
