c05c829fd2beea78b52798ef01b5db3f51b1a5af
Darya Lutuva <lutowa.darya@yandex.ru>
2020-08-31 00:19:18 +0300

    fixed: очищение попапа для добавления карточек (reset)

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,12 +5,12 @@ const popupOpenButton = document.querySelector('.edit-button');
 const popupAddCardOpenButton = document.querySelector('.profile__add-button');
 const popupAddCardCloseButton = document.querySelector('.popup__close-button_card');
 const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton = popup.querySelector('.popup__save-button');
+const popupSaveButton = popup.querySelector('.popup__button');
 const name = document.querySelector('.profile-info__name');
 const aboutYou = document.querySelector('.profile-info__activity');
-const inputName = popup.querySelector('.popup__form_input-name');
-const inputAboutYou = popup.querySelector('.popup__form_input-activity');
-const popupCardSaveButton = document.querySelector('.popup__save-button_card');
+const inputName = popup.querySelector('.popup__input_name');
+const inputAboutYou = popup.querySelector('.popup__input_activity');
+const popupCardSaveButton = document.querySelector('.popup__button_card');
 const popupPhoto = document.querySelector('.popup_photo');
 
 //каточки на станице
@@ -123,8 +123,8 @@ initialCards.forEach(function (item) {
 //Добавление карточки
 popupAddCard.addEventListener('submit', evt => {
     evt.preventDefault();
-    const nameElement = popupAddCard.querySelector('.popup__form_input-placename').value;
-    const linkElement = popupAddCard.querySelector('.popup__form_input-link').value;
+    const nameElement = popupAddCard.querySelector('.popup__input_placename').value;
+    const linkElement = popupAddCard.querySelector('.popup__input_link').value;
     
     addCard(nameElement, linkElement);
     popupAddCard.reset();
--- Scripts/validate.js
+++ Scripts/validate.js
@@ -1,17 +1,64 @@
+//функция для  проверки валидности на каждый ввод символа
+// Функция, которая добавляет класс с ошибкой
+// function showInputError(formElement, {inputSelector, ...rest}) {
+//     console.log(inputSelector);
+//     // inputSelector.classList.add('popup__input_type_error');
+//   };
+  
+// Функция, которая добавляет класс с ошибкой
+function showInputError(element) {
+    element.classList.add('popup__input_type_error');
+  };
+  
+  // Функция, которая удаляет класс с ошибкой
+  function hideInputError(element) {
+    element.classList.remove('popup__input_type_error');
+  };
+  
+  // Функция, которая проверяет валидность поля
+  function isValid() {
+      
+    if (!inputElement.validity.valid) {
+      // Если поле не проходит валидацию, покажем ошибку
+      showInputError(inputElement);
+    } else {
+      // Если проходит, скроем
+      hideInputError(inputElement);
+    }
+  };
+
+
+function setEventListener(formElement, {inputSelector, ...rest}) {
+    //создаю массив з всех инпутов
+    const inputList = Array.from(document.querySelectorAll(inputSelector)); 
+//обхожу весь массив инпутов и вывожу в консоль значеня свойства validity, при вводе значений в инпутах
+    inputList.forEach(function (inputElement) {
+        inputElement.addEventListener('input', function () {
+            isValid();
+          });    
+    });
+}
+
+function enableValidation ({formSelector, ...rest}) {
+    const getFormList = Array.from(document.querySelectorAll(formSelector));
+
+    getFormList.forEach(function (formElement) {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+       //вызов функции для  проверки валидности на каждый ввод символа
+        setEventListener(formElement, rest);  
+    });   
+}
+
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
-// enableValidation({
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_disabled',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__error_visible'
-//   });
-
-// const formElement = document.querySelector('.popup');
-
-// formElement.addEventListener('submit', function (evt) {
-//   evt.preventDefault();
-// });
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
