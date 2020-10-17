cfc08f1db9ac5ac7ee7fa3eddfc915977a42f75d
Darya Lutuva <lutowa.darya@yandex.ru>
2020-10-17 19:40:53 +0300

    форматирование

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,11 +7,12 @@ export default class PopupWithForm extends Popup {
     this._popupSaveButton = document.querySelector('.popup__button');
     this._formValues = [];
   }
+
   _getInputValues() {
     this._inputList = Array.from(
       this._popupElement.querySelectorAll('.popup__input')
     );
-    return this._formValues = this._inputList.map(input => input.value);
+    return this._formValues = this._inputList.map((input) => input.name = input.value);
   }
 
   setEventListeners() {
